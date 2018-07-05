6c003c4d3607d4a1cff66893eb3a43dfb5677a17
Sholom <boruchsteinmetz@outlook.com>
2018-07-05 11:46:05 -0400

    add appbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import AppBar from './components/AppBar'
 
 
 
@@ -20,7 +20,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <AppBar/>
       </div>
     );
   }
---
+++ src/components/AppBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
+
+function appBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant="title" color="inherit">
+            Tripply
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+
+export default withStyles(styles)(appBar);
