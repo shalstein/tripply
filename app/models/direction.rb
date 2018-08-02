e04f7f48b3829f5b67a0eef40e945967160cd380
Sholom <boruchsteinmetz@outlook.com>
2018-08-01 20:02:00 -0400

    add api returns weather info

--- app/models/direction.rb
+++ app/models/direction.rb
@@ -28,15 +28,38 @@ class Direction
 
     def parse_steps
         leg = @directions['routes'][0]['legs'][0]
+        meter_counter = 0
 
         steps = leg['steps'].map do |step|
-            step['html_instructions']
+            if meter_counter >= 1000 
+                weather = get_weather(step['end_location'])
+                meter_counter = 0
+                {html_instructions: step['html_instructions'], duration: step['duration']['text'], weather: weather}
+            else
+                meter_counter += step['duration']['value']
+                {html_instructions: step['html_instructions'], duration: step['duration']['text']}
+            end
         end
-
+        
 
         {distance: leg['distance']['text'], duration: leg['duration']['text'], steps: steps, destination: leg['end_address'],  origin: leg['start_address'], status: @directions['status']}
 
     end
+
+
+
+
+    def get_weather(coordinates)
+    binding.pry
+     response = Faraday.get("https://api.openweathermap.org/data/2.5/weather?lat=#{coordinates['lat']}&lon=#{coordinates['lng']}&APPID=#{ENV['WEATHER_API_KEY']}&units=metric")
+
+     weather = JSON.parse(response.body)
+
+     {weather: weather['weather'][0], temp: weather['main']['temp'], visibility: weather['visibility']}
+
+    end
+
+
 
 end
 
